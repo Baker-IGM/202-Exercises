1f6e98f6a5d6fb314e77aac121d8e17ae10a170c
Baker <eab6863@gmail.com>
2021-08-17 12:50:36 -0400

    Added starter code for random exercise

---
+++ Exercise 3/Assets/Scripts/RandomWalk.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// RandomWalk class
+// Placed on a walker prefab
+// Moves a random walker using randomized values
+
+public class RandomWalk : MonoBehaviour 
+{
+
+	// Class fields
+	// Will any class fields help the walker start and stop moving?
+
+
+	void Start () 
+	{
+		
+	}
+
+    void Update()
+    {
+        // Only call Walk() when a user presses the 'M' key
+        Walk();
+    }
+
+	/// <summary>
+	/// Walk()
+	/// Purpose: Calculates the position of a walker after he has taken a randomized step
+	/// Params:  none
+	/// Returns: none
+	/// </summary>
+	void Walk()
+	{
+		// Generate a random number
+
+		// Based on the number generated, the walker will move positively, negatively, or remain in place on the X axis
+
+		// Generate another random number
+
+		// Based on the number generated, the walker will move positively, negatively, or remain in place on the Z axis
+
+	}
+}
---
+++ Exercise 3/Assets/Scripts/TerrainGeneration.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TerrainGeneration class
+// Placed on a terrain game object
+// Generates a Perlin noise-based heightmap
+[RequireComponent (typeof(TerrainCollider))]
+public class TerrainGeneration : MonoBehaviour 
+{
+
+	private TerrainData myTerrainData;
+    [Tooltip ("The size of the terrain")]
+	public Vector3 worldSize = new Vector3(200, 50, 200);
+    [Tooltip("Number of vertices along X and Z axes")]
+    [Min (1)]
+    public int resolution = 129;
+	float[,] heightArray;
+
+
+	void Start () 
+	{
+		myTerrainData = gameObject.GetComponent<TerrainCollider>().terrainData;
+		myTerrainData.size = worldSize;
+		myTerrainData.heightmapResolution = resolution;
+
+		heightArray = new float[resolution, resolution];
+
+		// Fill the height array with values!
+		// Uncomment the Ramp and Perlin methods to test them out!
+		Flat(1.0f);
+		//Ramp();
+		//Perlin();
+
+		// Assign values from heightArray into the terrain object's heightmap
+		myTerrainData.SetHeights (0, 0, heightArray);
+	}
+	
+
+	void Update () 
+	{
+		
+	}
+
+	/// <summary>
+	/// Flat()
+	/// Assigns heightArray identical values
+	/// </summary>
+	void Flat(float value)
+	{
+		// Fill heightArray with 1's
+		for(int i = 0; i < resolution; i++)
+		{
+			for(int j = 0; j < resolution; j++)
+			{
+				heightArray [i, j] = value;
+			}
+		}
+	}
+		
+
+	/// <summary>
+	/// Ramp()
+	/// Assigns heightsArray values that form a linear ramp
+	/// </summary>
+	void Ramp()
+	{
+		// Fill heightArray with linear values
+
+
+	}
+
+	/// <summary>
+	/// Perlin()
+	/// Assigns heightsArray values using Perlin noise
+	/// </summary>
+	void Perlin()
+	{
+		// Fill heightArray with Perlin-based values
+
+
+	}
+}
